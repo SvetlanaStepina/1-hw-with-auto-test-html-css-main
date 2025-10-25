027f659cf9ae2c3096ea3a993326f535d2c0bbce
SvetlanaStepina <lady.svetlana.stepina@gmail.com>
2025-10-25 16:29:12 +0300

    Initial commit

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,10 +24,12 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const numbers = [1, 2, 3, 4, 5];
-
-const doubledNumbers = map(numbers, (element, index) => {
-  return element * 2;
-});
-
-console.log(doubledNumbers); // [2, 4, 6, 8, 10]
+const map = (array, callback) => {
+  const result = [];
+  
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i], i));
+  }
+  
+  return result;
+};
